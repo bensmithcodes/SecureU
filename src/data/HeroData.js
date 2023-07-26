186c0d91b277c4932ad9c40b59220bc4f0964aa1
Ben Smith <bencodes28@gmail.com>
2023-07-25 20:38:58 -0500

    changes made

---
+++ src/data/HeroData.js
@@ -0,0 +1,46 @@
+export const heroOne = {
+	reverse: true,
+	inverse: true,
+	topLine: {
+		text: 'Est. in 2023',
+	},
+	headline: "A growing startup company.",
+	description: 'We have designed and implemented the best cloud security solutions',
+	buttonLabel: 'Find Out More',
+	imgStart: 'start',
+	img: './assets/svg/Deal.svg',
+	start: 'true',
+};
+
+export const heroTwo = {
+	reverse: false,
+	inverse: false,
+	topLine: {
+		text: 'Designed & Developed',
+	},
+	headline: 'Our best practices',
+	description: ' Clients had the best experience working with us!',
+	buttonLabel: 'View Project Here',
+
+	linkTo: '/more',
+	imgStart: 'start',
+	img: './assets/svg/Connection.svg',
+	start: 'true',
+};
+
+export const heroThree = {
+	reverse: true,
+	inverse: true,
+	topLine: {
+		text: 'Highly reputed brand',
+	},
+	headline: 'Why choose us? ',
+	description:
+		' Very experienced and we are  allowed to use the most innovative technologies and methodologies',
+	buttonLabel: 'View Project',
+
+	linkTo: '/download',
+	imgStart: '',
+	img: './assets/svg/ChartUp.svg',
+	start: 'true',
+};
